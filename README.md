6faf7a0ec9ea0a74126200e3beb21d364236ada6
Gabriel Vasconcelos <gfreivasc@gmail.com>
2016-12-30 18:28:29 -0300

    Adicionando README e um pouco mais de integridade ao banco de dados

---
+++ README.md
@@ -0,0 +1,22 @@
+Price Monitor
+=============
+Veja se aquela promoção é mesmo uma promoção
+
+Lojas
+-----
++ KaBuM
+
+Como rodar?
+-----------
+Tenha um banco de dados definido e modifique `settings.py` de acordo
+
+Depois rode
+```bash
+$ scrapy crawl monitor
+```
+
+Espere e veja os dados populados. That's all, folks.
+
+Legal
+-----
+Eu posso fazer isso.
--- pipelines.py
+++ pipelines.py
@@ -11,7 +11,7 @@ class PriceMonitorPipeline(object):
 
     def process_item(self, item, spider):
         session = self.Session()
-        product = session.query(Product).filter_by(name=item['name']).first()
+        product = session.query(Product).filter_by(name=item['url']).first()
         if product is None:
             product = Product(
                 name=item['name'],
