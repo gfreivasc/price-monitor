0abc5160aa6ffbb7049f2ddb376957d08776480e
Gabriel Vasconcelos <gfreivasc@gmail.com>
2017-01-06 19:57:41 -0300

    Adjusting API endpoints

--- price_monitor/app/app.py
+++ price_monitor/app/app.py
@@ -1,6 +1,6 @@
 from flask import Flask, jsonify
 from price_monitor.db.database import db_connect, db_session
-from price_monitor.db.models import Product, Price
+from price_monitor.db.models import Product
 
 app = Flask(__name__)
 
@@ -8,11 +8,39 @@ app = Flask(__name__)
 @app.route('/')
 def index():
     session = db_session(db_connect())
-    query = session.query(Product.name, Product.category).all()
+    query = session.query(Product).all()
     data = {}
     for item in query:
         category = data.get(item.category, [])
-        category.append(item.name)
+        category.append({
+            'id': item.id,
+            'name': item.name,
+            'rating': item.rating,
+            'last_price': item.last_price,
+            'on_last_scan': item.on_last_scan
+        })
         data[item.category] = category
+    session.close()
+    return jsonify(**data)
+
+
+@app.route('/p/<int:product_id>')
+def get_product(product_id):
+    session = db_session(db_connect())
+    product = session.query(Product).get(product_id)
+    data = {
+        'id': product.id,
+        'url': product.url,
+        'name': product.name,
+        'rating': product.rating,
+        'last_price': product.last_price,
+        'on_last_scan': product.on_last_scan,
+        'prices': []
+    }
+    for price in product.prices:
+        data['prices'].append({
+            'value': price.value,
+            'when_read': price.when_read
+        })
     session.close()
     return jsonify(**data)
